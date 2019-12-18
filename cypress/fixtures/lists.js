eba4662ce65bce80d5303b54691acdeeb00ca06a
ivanori1 <coric.ivan@yandex.com>
2019-12-18 21:09:53 +0100

    Error list update

--- cypress/fixtures/lists.js
+++ cypress/fixtures/lists.js
@@ -8,13 +8,10 @@ const listOfGames = [
 ];
 exports.listOfGames = listOfGames;
 
-
 const listOfErrors = [
-  "Dota 2",
-  "Fifa 2019",
-  "Rainbow Six Siege",
-  "Underlords",
-  "Teamfight Tactics",
-  "Chess Rush"
+  "No spaces, more than 6 chars!",
+  "Invalid E-mail!",
+  "Invalid Password!",
+  "Invalid Repeat Password!"
 ];
 exports.listOfErrors = listOfErrors;
